9cec9aea4df577131651d0c492fc97bf4a1b7119
MotNosniktaw <tomwatkinson90gmail.com>
2020-12-19 20:10:57 +0000

    create jpeg from sent array

--- whiteboard/whiteboard.js
+++ whiteboard/whiteboard.js
@@ -1,5 +1,6 @@
 const NodeCache = require("node-cache");
 const { uuid } = require("uuidv4");
+const jpeg = require("jpeg-js");
 const cache = new NodeCache();
 
 function intialiseWhiteboard(io) {
@@ -16,6 +17,41 @@ function intialiseWhiteboard(io) {
     socket.on("load", ({ key, sub }) => {
       const data = cache.get(key);
       io.emit("load", { data, key, sub });
+    });
+    socket.on("save", ({ data }) => {
+      var width = data.length;
+      var height = data[0].length;
+
+      console.log({ data });
+
+      var frameData = Buffer.alloc(width * height * 4);
+      console.log(frameData, typeof frameData);
+
+      var counter = 0;
+
+      data.forEach((row) => {
+        row.forEach((cell) => {
+          var rgbData = [];
+          let rgb = [255, 255, 255];
+          if (cell !== "") {
+            rgb = cell
+              .replace("rgb(", "")
+              .replace(")", "")
+              .replace(" ", "")
+              .split(",");
+            console.log({ cell });
+          }
+          frameData[counter++] = Number(rgb[0]).toString(16);
+          frameData[counter++] = Number(rgb[1]).toString(16);
+          frameData[counter++] = Number(rgb[2]).toString(16);
+          frameData[counter++] = Number(255).toString(16);
+        });
+      });
+
+      console.log({ frameData });
+      var imageData = jpeg.encode({ data: frameData, width, height });
+
+      console.log({ imageData });
     });
     socket.on("disconnect", (thing) => {
       console.log("user disconnected");
